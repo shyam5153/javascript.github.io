f142634719be0b8650505c464b93431b76cc86bb
shyam5153 <shyam91760@gmail.com>
2024-10-01 22:13:51 +0530

    Add files via upload

---
+++ js file/script.js
@@ -0,0 +1,47 @@
+var msg ="Shyam"
+var msg ="welcome to Java Script";
+
+// Alearting
+alert(msg);
+
+
+//Logging
+console.log("msg"); 
+
+let msg ="Shyam"
+let msg ="welcome to Java Script let's start from basics";
+
+// Alearting
+alert(msg);
+
+
+//Logging
+console.log("msg"); 
+
+  let number5 ="5";
+  console.log(result);
+
+  const name = "shyam";
+  const age = "23";
+
+  let msg ="Hello my name is " + name + " i born on " + dob + " I'm learning js"
+  
+  /loggging
+  console.log(msg);
+
+ /*
+     synchronous & Asynchronous| js
+ /*
+ 
+ console.log("\n synchronous process ");
+ console.log("js-1");
+ console.log("js-2");
+ console.log("js-3");
+
+
+ console.log("\n Asynchronous process ");
+ console.log("js-1");
+ setTimeout(()= >{
+   console.log("js-2");
+ },1000);
+ console.log(js-3);
